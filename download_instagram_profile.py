b2dd31c10223b4b80c5d977a11439844b2014d28
indianacodes <thxcodes@gmail.com>
2021-06-25 14:57:52 -0300

    python - bot to download instagram profile

--- download_instagram_profile.py
+++ download_instagram_profile.py
@@ -3,7 +3,7 @@ import instaloader
 
 insta = instaloader.Instaloader()
 
-allusers = ['thisisbillgates', 'zuck', 'linustorvalds', 'stevejobs']
+allusers = ['thisisbillgates', 'zuck', 'linustorvalds', 'stevejobsok']
 
 for user in allusers:
     insta.download_profile(user, profile_pic_only=True)
